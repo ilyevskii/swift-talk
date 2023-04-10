b95d36e4c48bb891574811ff26b10ab3872c7916
ilyevskii <ilya.andreevskii2004@gmail.com>
2023-04-10 20:25:40 +0300

    Added the ability to choose menu items

--- client/src/components/ModalMenu/ModalMenu.jsx
+++ client/src/components/ModalMenu/ModalMenu.jsx
@@ -2,16 +2,31 @@ import React, { useState } from 'react';
 import './modalmenu.css';
 import {PersonOutlineOutlined, SettingsOutlined, InfoOutlined} from "@mui/icons-material";
 
-export default function ModalMenu() {
+export default function ModalMenu(props) {
 
     return (
         <>
             <div className="modal">
                 <div className="menu">
                     <ul>
-                        <li className="menu-item"><PersonOutlineOutlined className="menu-item-icon"/> Contacts</li>
-                        <li className="menu-item"><SettingsOutlined className="menu-item-icon"/> Settings</li>
-                        <li className="menu-item"><InfoOutlined className="menu-item-icon"/> About</li>
+                        <li className="menu-item" onClick={() => {
+                            props.changeMenuItem('contacts')
+                            props.toggleButton(true);
+                        }}>
+                            <PersonOutlineOutlined className="menu-item-icon"/> Contacts
+                        </li>
+                        <li className="menu-item" onClick={() => {
+                            props.changeMenuItem('settings')
+                            props.toggleButton(true)
+                        }}>
+                            <SettingsOutlined className="menu-item-icon"/> Settings
+                        </li>
+                        <li className="menu-item" onClick={() => {
+                            props.changeMenuItem('about')
+                            props.toggleButton(true)
+                        }}
+                            ><InfoOutlined className="menu-item-icon"/> About
+                        </li>
                     </ul>
                     <p className="description">by @ilyevskii.</p>
                 </div>
