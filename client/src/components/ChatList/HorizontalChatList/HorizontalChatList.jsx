09dcebd9b59e95e63799e3d82fe377b1094aff14
ilyevskii <ilya.andreevskii2004@gmail.com>
2023-04-10 03:54:54 +0300

    Backend connected, now chats are taken from the db

--- client/src/components/ChatList/HorizontalChatList/HorizontalChatList.jsx
+++ client/src/components/ChatList/HorizontalChatList/HorizontalChatList.jsx
@@ -1,29 +1,21 @@
 import './horizontalchatlist.css';
-import { useEffect, useState } from "react";
 
-interface Chat {
-    id: number;
-    name: string;
-}
+export default function HorizontalChatList(props) {
 
-interface Props {
-    chats: Chat[];
-}
-
-export default function HorizontalChatList({ chats }: Props) {
-    useEffect(() => {
-        console.log(chats);
-    }, [])
+    const {socket, chats, setSelectedChat} = props;
 
     const image_url = 'https://avatars.mds.yandex.net/i?id=5d8db0440aae4c3265492d1b3f8de64dddf64453-8342484-images-thumbs&n=13'
-
     return (
         <>
             <div className="horizontal-chat-list">
                 {chats.length ?
                     <div className="horizontal-chat-list-content">
-                        {chats.map((chat: Chat) => (
-                            <div key={chat.id} className="horizontal-chat-preview">
+                        {chats.map((chat) => (
+                            <div key={chat._id} className="horizontal-chat-preview" onClick={() =>
+                            {
+                                socket.emit("join_chat", chat._id)
+                                setSelectedChat(chat)
+                            }}>
                                 <img src={image_url} alt={chat.name} className="horizontal-chat-image" />
                                 <h3 className="horizontal-chat-name">{chat.name}</h3>
                             </div>
@@ -37,3 +29,4 @@ export default function HorizontalChatList({ chats }: Props) {
         </>
     );
 }
+
--- client/src/components/ChatList/VerticalChatList/VerticalChatList.jsx
+++ client/src/components/ChatList/VerticalChatList/VerticalChatList.jsx
@@ -1,102 +1,8 @@
 import './verticalchatlist.css';
-import {useAuth} from "../../../contexts/Auth/AuthContext";
 
-export default function VerticalChatList({user_id}) {
+export default function VerticalChatList(props) {
 
-    const chats = [
-        {
-            id: 1,
-            name: 'илья.',
-            lastMessage: 'Привет! Как дела?'
-        },
-        {
-            id: 2,
-            name: 'илья.',
-            lastMessage: 'Ооооо'
-        },
-        {
-            id: 3,
-            name: 'илья.',
-            lastMessage: 'аплллтпдпаодлпалд'
-        }
-        ,
-        {
-            id: 4,
-            name: 'илья.',
-            lastMessage: 'аплллтпдпаодлпалд'
-        }
-        ,
-        {
-            id: 5,
-            name: 'илья.',
-            lastMessage: 'аплллтпдпаодлпалд'
-        }
-        ,
-        {
-            id: 6,
-            name: 'илья.',
-            lastMessage: 'аплллтпдпаодлпалд'
-        }
-        ,
-        {
-            id: 7,
-            name: 'илья.',
-            lastMessage: 'аплллтпдпаодлпалд'
-        }
-        ,
-        {
-            id: 8,
-            name: 'илья.',
-            lastMessage: 'аплллтпдпаодлпалд'
-        }
-        ,
-        {
-            id: 9,
-            name: 'илья.',
-            lastMessage: 'аплллтпдпаодлпалд'
-        }
-        ,
-        {
-            id: 10,
-            name: 'илья.',
-            lastMessage: 'аплллтпдпаодлпалд'
-        }
-        ,
-        {
-            id: 11,
-            name: 'илья.',
-            lastMessage: 'аплллтпдпаодлпалд'
-        },
-        {
-            id: 12,
-            name: 'илья.',
-            lastMessage: 'аплллтпдпаодлпалд'
-        }
-        ,
-        {
-            id: 13,
-            name: 'илья.',
-            lastMessage: 'аплллтпдпаодлпалд'
-        }
-        ,
-        {
-            id: 113,
-            name: 'илья.',
-            lastMessage: 'аплллтпдпаодлпалд'
-        }
-        ,
-        {
-            id: 3345,
-            name: 'илья.',
-            lastMessage: 'аплллтпдпаодлпалд'
-        }
-        ,
-        {
-            id: 33645,
-            name: 'илья.',
-            lastMessage: 'аплллтпдпаодлпалд'
-        }
-    ]
+    const {socket, chats, setSelectedChat} = props;
 
     const image_url = 'https://avatars.mds.yandex.net/i?id=5d8db0440aae4c3265492d1b3f8de64dddf64453-8342484-images-thumbs&n=13'
     return (
@@ -105,11 +11,26 @@ export default function VerticalChatList({user_id}) {
                 {chats.length ?
                     <div className="vertical-chat-list-content">
                     {chats.map((chat) => (
-                        <div key={chat.id} className="vertical-chat-preview">
+                        <div key={chat._id} className="vertical-chat-preview">
                             <img src={image_url} alt={chat.name} className="vertical-chat-image" />
-                            <div className="vertical-chat-info">
+
+                            <div className="vertical-chat-info" onClick={() =>
+                            {
+                                socket.emit("join_chat", chat._id)
+                                setSelectedChat(chat)
+                            }}>
                                 <h3 className="vertical-chat-name">{chat.name}</h3>
-                                <p className="vertical-chat-last-message">{chat.lastMessage}</p>
+                                {Object.keys(chat.last_message).length ?
+                                    <>
+                                        <p className="vertical-chat-last-message">
+                                            {chat.last_message.text}
+                                            <span>{chat.last_message.time}</span>
+                                        </p>
+                                    </>
+                                    :
+                                    <p className="vertical-chat-last-message">No messages yet.</p>
+                                }
+
                             </div>
                         </div>
                     ))}
