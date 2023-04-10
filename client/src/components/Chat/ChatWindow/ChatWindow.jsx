4e9cea1eb1cb40ad991cbf1a084809bcb3b8c0f8
ilyevskii <ilya.andreevskii2004@gmail.com>
2023-04-10 03:53:34 +0300

    Added MessageForm

--- client/src/components/Chat/ChatWindow/ChatWindow.jsx
+++ client/src/components/Chat/ChatWindow/ChatWindow.jsx
@@ -1,46 +1,83 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import ChatHeader from '.././ChatHeader/ChatHeader';
 import ChatMessage from '.././ChatMessage/ChatMessage';
+import {MessageForm} from "../MessageForm/MessageForm";
+import {useState} from "react";
+import {chatRepo} from "../../../serializer";
 import './chatwindow.css';
 
-const messages = [
-    {
-        id: 1,
-        text: 'JdjfhghkskfjgfjgJdjfhghkskfjgfjgJdjfhghkskfjgfjgJdjfhghkskfjgfjgJdjfhghkskfjgfjgJdjfhghkskfjgfjgJdjfhghkskfjgfjgJdjfhghkskfjgfjgJdjfhghkskfjgfjgJdjfhghkskfjgfjg',
-        sender_id: 1
-    },
-    {
-        id: 2,
-        text: 'Hi',
-        sender_id: 2
-    },
-    {
-        id: 3,
-        text: 'Jdjfhghkskfjgfjg',
-        sender_id: 1
-    },
-    {
-        id: 4,
-        text: 'Hi',
-        sender_id: 1
-    }
-]
+export default function ChatWindow(props) {
 
-export default function ChatWindow({ chat, user_id }) {
+    const [messages, setMessages] = useState([]);
+
+    const { socket, chat, user_id } = props;
+
+
+    useEffect(() => {
+        setMessages([]);
+
+        async function getChatMessages() {
+            const messages = await chatRepo.getChatMessages(chat._id);
+            setMessages(messages);
+        }
+
+        getChatMessages();
+    }, [chat])
+
+    async function sendMessage(currentMessage) {
+
+        if (currentMessage) {
+            const messageData = {
+                chat_id: chat._id,
+                sender_id: user_id,
+                text: currentMessage,
+            }
+
+            setMessages((messages) =>
+                [...messages,
+                    {
+                        _id: Math.floor(Math.random() * 10000000),
+                        text: currentMessage,
+                        sender_id: user_id,
+                        time: new Date(Date.now()).getHours().toString().padStart(2, "0")
+                            + ":"
+                            + new Date(Date.now()).getMinutes().toString().padStart(2, "0")
+                    }])
+
+            await socket.emit("send_message", messageData);
+        }
+    }
+
+    useEffect(() => {
+        socket.on("receive_message", () => {
+
+            async function setLastMessage() {
+                const lastMessage = await chatRepo.getLastMessage(chat._id);
+                if (lastMessage.sender_id !== user_id) {
+                    setMessages((messages) => [...messages, lastMessage]);
+                }
+            }
+
+            setLastMessage();
+        })
+
+    }, [socket]);
+
     const image_url = 'https://avatars.mds.yandex.net/i?id=5d8db0440aae4c3265492d1b3f8de64dddf64453-8342484-images-thumbs&n=13'
     return (
         <div className="chat-window">
-            <ChatHeader chatName={'Test'} chatPhoto={image_url} />
+            <ChatHeader chatName={chat.name} chatPhoto={image_url} />
             <div className="chat-messages">
                 {messages.map((message) => (
                     <ChatMessage
-                        key={message.id}
+                        key={message._id}
                         message={message.text}
-                        isMyMessage={message.sender_id === 1}
-                        time={'11:11'}
+                        isMyMessage={message.sender_id === user_id}
+                        time={message.time}
                     />
                 ))}
             </div>
+            <MessageForm submitFunc={sendMessage}/>
         </div>
     );
 };
